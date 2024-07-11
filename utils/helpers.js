68455e20b15d912467fb5c8173fd2d99f16e8d1f
unknown <dpippin09@gmail.com>
2024-07-10 20:03:02 -0600

    utils and helpers files fixes

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,32 +1,44 @@
-function formatDate(date) {
-    return date.toISOString().split('T')[0];
+const { format } = require("mysql2");
+
+module.exports = {
+  format_date: () => {
+    const currentDate = new Date();
+    return `${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
   }
-  
-  // Capitalize the first letter of each word in a string
-  function capitalizeWords(str) {
-    return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
   }
+
+
+
+
+// function formatDate(date) {
+//     return date.toISOString().split('T')[0];
+//   }
   
-  // Check if an email is valid
-  function isValidEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(([^<>()[\]\\.,;:\s@"]+\.)+[^<>()[\]\\.,;:\s@"]{2,})$/i;
-    return re.test(String(email).toLowerCase());
-  }
+//   // Capitalize the first letter of each word in a string
+//   function capitalizeWords(str) {
+//     return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+//   }
   
-  // Debounce function to limit the rate at which a function can fire
-  function debounce(func, wait, immediate) {
-    let timeout;
-    return function() {
-      const context = this, args = arguments;
-      const later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      const callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
-  }
+//   // Check if an email is valid
+//   function isValidEmail(email) {
+//     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(([^<>()[\]\\.,;:\s@"]+\.)+[^<>()[\]\\.,;:\s@"]{2,})$/i;
+//     return re.test(String(email).toLowerCase());
+//   }
   
-  module.exports = { formatDate, capitalizeWords, isValidEmail, debounce };
+//   // Debounce function to limit the rate at which a function can fire
+//   function debounce(func, wait, immediate) {
+//     let timeout;
+//     return function() {
+//       const context = this, args = arguments;
+//       const later = function() {
+//         timeout = null;
+//         if (!immediate) func.apply(context, args);
+//       };
+//       const callNow = immediate && !timeout;
+//       clearTimeout(timeout);
+//       timeout = setTimeout(later, wait);
+//       if (callNow) func.apply(context, args);
+//     };
+//   }
+  
+//   module.exports = { formatDate, capitalizeWords, isValidEmail, debounce };
